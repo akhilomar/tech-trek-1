67b1586ca1fba2c46952520025a17662bb4e893d
Prakhar Varshney <prakhar.varshney33@gmail.com>
2019-08-10 20:34:58 +0530

    create UserProfile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='profile'
+    )
+    payment_done = models.BooleanField(default=False)
+    last_solved = models.DateTimeField(auto_now_add=True)
+    current_question = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.user.username
