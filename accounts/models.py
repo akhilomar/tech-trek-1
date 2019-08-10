d09b31371a8c63a6e0cf6824b56282586e1d70f0
Prakhar Varshney <prakhar.varshney33@gmail.com>
2019-08-10 20:40:00 +0530

    add user post_save receiver

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from rest_framework.authtoken.models import Token
 
 class UserProfile(models.Model):
     user = models.OneToOneField(
@@ -13,3 +16,8 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
