a6b04731f324e978b7b501cea81bd4e4b05671b2
Prakhar Varshney <prakhar.varshney33@gmail.com>
2019-08-11 16:07:09 +0530

    add accounts api views

---
+++ accounts/api/views.py
@@ -0,0 +1,66 @@
+from django.contrib.auth.models import User
+from rest_framework import generics, views
+
+from .serializers import (
+    UserListSerializer,
+    UserDetailSerializer,
+    UserCreateSerializer,
+    UserLoginSerializer,
+    UserTokenSerializer,
+    UserUpdateSerializer
+)
+
+class UserCreateAPIView(generics.CreateAPIView):
+    serializer_class = UserCreateSerializer
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+
+class UserDetailAPIView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserDetailSerializer
+    lookup_field = 'username'
+    permission_classes = [AllowAny]
+
+class UserDeleteAPIView(generics.DestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserDetailSerializer
+    lookup_field = 'username'
+    # permission_classes = [IsOwnerOrAdminOrReadOnly]
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserUpdateSerializer
+    lookup_field = 'username'
+
+class UserListAPIView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserListSerializer
+
+class UserLoginAPIView(views.APIView):
+    permission_classes = [AllowAny]
+    serializer_class = UserLoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = UserTokenSerializer(
+            data=request.data,
+            context={'request': request}
+        )
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.validated_data['user']
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({
+                'token': token.key,
+                'username': user.username,
+            }, status=HTTP_200_OK)
+
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+class UserLogoutAPIView(views.APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        try:
+            request.user.auth_token.delete()
+            return Response(status=HTTP_200_OK)
+        except:
+            return Response(status=HTTP_400_BAD_REQUEST)
