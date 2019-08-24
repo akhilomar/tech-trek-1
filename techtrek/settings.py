3b2e0c4f1f076a2670040393679a3df8c37b79ef
Prakhar Varshney <prakhar.varshney33@gmail.com>
2019-08-24 17:48:38 +0530

    add token authentication class

--- techtrek/settings.py
+++ techtrek/settings.py
@@ -28,6 +28,17 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# REST Framework
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
 
 # Application definition
 
