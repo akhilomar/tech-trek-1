460584df5cf54817df9b1f2f276d9a6e433d9f9d
Prakhar Varshney <prakhar.varshney33@gmail.com>
2019-08-27 11:36:21 +0530

    handle RelatedObjectDoesNotExist exception

--- questions/api/views.py
+++ questions/api/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics, views
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from questions.models import Question
+from accounts.models import UserProfile
 
 from questions.api.serializers import (
     QuestionDetailSerializer,
@@ -12,8 +13,11 @@ class GetQuestion(views.APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, format=None):
-        u = request.user
-        player = u.profile
+        try:
+            u = request.user
+            player = u.profile
+        except UserProfile.DoesNotExist:
+            return Response({"error": "userprofile does not exist"})
         question = Question.objects.get(level=player.current_question)
         serializer = QuestionDetailSerializer(question)
         return Response(serializer.data)
@@ -22,13 +26,20 @@ class SubmitQuestion(views.APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, format=None):
-        u = request.user
-        player = u.profile
+        try:
+            u = request.user
+            player = u.profile
+        except UserProfile.DoesNotExist:
+            return Response({"error": "userprofile does not exist"})
         print("{} entered answer {}".format(u.username, request.data['answer']))
+        
+        # TODO: Add utility function
         question = Question.objects.get(level=player.current_question)
         if (request.data['answer'] == question.answer):
             player.current_question = player.current_question + 1
             player.save()
+
+            # TODO: Update Leaderboard
             is_correct = True
         else:
             is_correct = False
