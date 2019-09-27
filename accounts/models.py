62a299f49ed39c20a1ea4e6013ea395ea41090d2
Prakhar Varshney <prakhar.varshney33@gmail.com>
2019-09-27 20:02:33 +0530

    add score field

--- accounts/models.py
+++ accounts/models.py
@@ -5,6 +5,7 @@ class Player(AbstractUser):
     is_paid = models.BooleanField(default=False)
     last_solved = models.DateTimeField(auto_now_add=True)
     current_question = models.IntegerField(default=1)
-
+    score = models.IntegerField(default=0)
+    
     def __str__(self):
         return self.username
