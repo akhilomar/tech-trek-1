52e243bb0095088d4a7063b1c19522dd8580662e
Prakhar Varshney <prakhar.varshney33@gmail.com>
2019-10-07 16:21:08 +0530

    minor fixes

--- accounts/admin.py
+++ accounts/admin.py
@@ -10,7 +10,15 @@ class PlayerChangeForm(UserChangeForm):
 class PlayerUserAdmin(UserAdmin):
     form = PlayerChangeForm
     fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('is_paid', 'current_question', 'last_solved', 'unlock_time', 'avatar', 'avatar_no')}),
+        (None, {'fields': (
+            'is_paid',
+            'current_question',
+            'last_solved',
+            'unlock_time',
+            'avatar',
+            'avatar_no'
+            )
+        }),
     )
 
 admin.site.register(Player, PlayerUserAdmin)
--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -16,27 +16,3 @@ urlpatterns = [
     path('api/list/', PlayerListAPIView.as_view(), name='player-list'),
     path('api/', PlayerDashboardAPIView.as_view(), name='dashboard'),
 ]
-
-# # from django.conf.urls import url
-
-# # from django.contrib import admin
-
-# from accounts.api.views import (
-#     UserCreateAPIView,
-#     UserDetailAPIView,
-#     UserUpdateAPIView,
-#     UserListAPIView,
-#     UserDeleteAPIView,
-#     UserLoginAPIView,
-#     UserLogoutAPIView
-# )
-
-# urlpatterns = [
-#     path('', UserListAPIView.as_view(), name='user-list'),
-#     path('register/', UserCreateAPIView.as_view(), name='user-register'),
-#     path('login/', UserLoginAPIView.as_view(), name='user-login'),
-#     path('logout/', UserLogoutAPIView.as_view(), name='user-logout'),
-#     path('<slug:username>/', UserDetailAPIView.as_view(), name='user-detail'),
-#     path('<slug:username>/edit/', UserUpdateAPIView.as_view(), name='user-update'),
-#     path('<slug:username>/delete/', UserDeleteAPIView.as_view(), name='user-delete'),
-# ]
